29d785299b72f63ff3c98b2bea990dc9d3e19b55
elitza-vasileva <elitza.vasileva@hotmail.com>
2026-08-14 18:48:15 +0300

    Correct DataFast website ID

--- web/shared/analytics.js
+++ web/shared/analytics.js
@@ -4,7 +4,7 @@ const PRODUCTION_HOSTS = new Set(["timeonx.com", "www.timeonx.com"]);
 
 if (PRODUCTION_HOSTS.has(window.location.hostname)) {
   void initDataFast({
-    websiteId: "dfid_9gNnnCfCuF997NF3DxbQP",
+    websiteId: "dfid_9gNnnCfCuF997NF3DxBQP",
     autoCapturePageviews: true,
   }).catch(() => {
     // Analytics must never interfere with the website experience.
